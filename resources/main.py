4de40dfd74edb1940192e3650d4110b67f704471
Thierry Laforge <21laforgth@gmail.com>
2023-04-30 16:50:19 -0600

    Update method to find last row

--- resources/main.py
+++ resources/main.py
@@ -10,7 +10,7 @@ def read_code(message):
 
     while True:
         ret, frame = cam.read()
-        data, bbox, straight_qrcode = reader.detectAndDecode(frame)
+        data, _, _ = reader.detectAndDecode(frame)
 
         frame = cv2.flip(frame, 1)
         cv2.putText(frame, message, [10, 30], cv2.FONT_HERSHEY_DUPLEX, 1, [0, 0, 0], 1, cv2.LINE_AA)
@@ -31,24 +31,22 @@ with open("settings.txt", "r") as settings:
     lines = [line.rstrip() for line in settings if line != "\n"]
 
 try:
+    # TODO: Find a better way to dynamically determine the amount of temp students.
     temps = [entry.split(":")[1].strip() for entry in lines[0:15:]]
     delay = int(lines[15].split(":")[1].strip())
     room = int(lines[16].split(":")[1].strip())
 
 except ValueError:
-    print("Unexpected value for delay and/or room number in settings.txt. Please check again and run.")
+    print("Unexpected value for delay and/or room number in settings.txt. Please check and run again.")
     exit(1)
-
-temps_dict = {f"student{i + 1}": val for i, val in enumerate(temps)}
 
 client = gspread.service_account("resources\\cbtracking-385301-ac9bc3a18813.json")
 sheet = client.open("Chromebook Tracker").worksheet(f"SF{room}")
 
-status_cells = {f"SF{room}-1": "G2", f"SF{room}-2": "H2", f"SF{room}-3": "I2", f"SF{room}-4": "J2", f"SF{room}-5": "K2", f"SF{room}-6": "L2"}
+status_cells = {f"SF{room}-1": "G2", f"SF{room}-2": "H2", f"SF{room}-3": "I2", f"SF{room}-4": "J2", f"SF{room}-5": "K2",f"SF{room}-6": "L2"}
 status_rev = {"OUT": "IN", "IN": "OUT"}
-
+temps_dict = {f"student{i + 1}": val for i, val in enumerate(temps)}
 date = datetime.today()
-
 chromebook = read_code("Please show chromebook")
 sleep(delay)
 name = read_code("Please show ID")
@@ -57,8 +55,7 @@ try:
     last_row = sheet.findall(f"{date.month}/{date.day}/{date.year}")[-1].row + 1
 
 except IndexError:
-    print("Empty spreadsheet. Running first row")
-    last_row = 2
+    last_row = len(sheet.col_values(1)) + 1
 
 try:
     sheet.update(status_cells[chromebook], status_rev[sheet.acell(status_cells[chromebook]).value])
@@ -74,3 +71,4 @@ try:
 
 except KeyError:
     print("Error writing to the sheet. Is the class room correct?")
+    exit(2)
