24bf3bb3a1b0a303c316e41ad580213d3aa5fa64
Thierry Laforge <21laforgth@gmail.com>
2023-05-07 18:33:58 -0600

    Add option to generate qr codes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import json
 import qrcode
 import hashlib
+from sys import exit
 from datetime import datetime
 from time import sleep
 
@@ -14,6 +15,8 @@ def read_json(path: str) -> dict:
 
     except FileNotFoundError:
         print("File could not be found.")
+        print("Exiting in 10 seconds (You can close this window)")
+        sleep(10)
         exit(5)
 
 
@@ -24,6 +27,8 @@ def read_file(path: str) -> list:
 
     except FileNotFoundError:
         print("File could not be found.")
+        print("Exiting in 10 seconds (You can close this window)")
+        sleep(10)
         exit(5)
 
 
@@ -34,29 +39,9 @@ def write_file(path: str, obj: dict):
 
     except FileNotFoundError:
         print("File could not be found.")
+        print("Exiting in 10 seconds (You can close this window)")
+        sleep(10)
         exit(5)
-
-
-def read_code(message: str):
-    cam = cv2.VideoCapture(0)
-    reader = cv2.QRCodeDetector()
-
-    while True:
-        _, frame = cam.read()
-        data, _, _ = reader.detectAndDecode(frame)
-
-        frame = cv2.flip(frame, 1)
-        cv2.putText(frame, message, [10, 30], cv2.FONT_HERSHEY_DUPLEX, 1, [0, 0, 0], 1, cv2.LINE_AA)
-        cv2.imshow("Haha funny", frame)
-        cv2.setWindowProperty("Haha funny", cv2.WND_PROP_TOPMOST, 1)
-
-        if len(data) > 0:
-            return data
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cam.release()
-            cv2.destroyAllWindows()
-            exit(0)
 
 
 def generate_qr_codes():
@@ -73,7 +58,38 @@ def generate_qr_codes():
     write_file(r"resources/validation.json", {v: k for k, v in codes.items()})
 
 
+def read_code(message: str):
+    cam = cv2.VideoCapture(0)
+    reader = cv2.QRCodeDetector()
+
+    while True:
+        _, frame = cam.read()
+        data, _, _ = reader.detectAndDecode(frame)
+
+        frame = cv2.flip(frame, 1)
+        cv2.putText(frame, message, [10, 30], cv2.FONT_HERSHEY_DUPLEX, 1, [0, 0, 0], 1, cv2.LINE_AA)
+        cv2.putText(frame, "Press 'g' to generate QR codes and 'q' to quit.", [10, 40], cv2.FONT_HERSHEY_DUPLEX, 1, [0, 0, 0], 1, cv2.LINE_AA)
+        cv2.imshow("Scanning...", frame)
+        cv2.setWindowProperty("Scanning...", cv2.WND_PROP_TOPMOST, 1)
+
+        if len(data) > 0:
+            return data
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            cam.release()
+            cv2.destroyAllWindows()
+            exit(0)
+
+        elif key == ord('g'):
+            cam.release()
+            cv2.destroyAllWindows()
+            generate_qr_codes()
+            exit(0)
+
+
 if __name__ == "__main__":
+    print("Reading settings...")
     lines = read_file("resources/settings.txt")
     temps = []
     delay = -1
@@ -96,8 +112,11 @@ if __name__ == "__main__":
 
     except ValueError:
         print("Unexpected value for delay and/or room number in settings.txt. Please check and run again.")
+        print("Exiting in 10 seconds (You can close this window)")
+        sleep(10)
         exit(2)
 
+    print("Starting gspread client...")
     client = gspread.service_account("resources/cbtracking-385301-ac9bc3a18813.json")
     sheet = client.open("Chromebook Tracker").worksheet(f"SF{room}")
 
@@ -105,12 +124,14 @@ if __name__ == "__main__":
     status_rev = {"OUT": "IN", "IN": "OUT"}
     temps_dict = {f"student{i + 1}": val.split(":")[1].strip() for i, val in enumerate(temps)}
     date = datetime.today()
+    print("Opening camera. Smile!")
     chromebook = read_code("Please show chromebook")
     sleep(delay)
     name = read_code("Please show ID")
     cv2.destroyAllWindows()
     decrypt = read_json("resources/validation.json")
 
+    print("Verifying inputs...")
     if name not in decrypt:
         print("QR code not recognized. Please get teacher to look into this.")
         exit(3)
@@ -123,6 +144,7 @@ if __name__ == "__main__":
         # Use this method as a backup. Gets a list of all elements in a column and gets the length.
         last_row = len(sheet.col_values(1)) + 1
 
+    print("Info good! Updating sheet...")
     name = decrypt[name]
     if name in temps_dict:
         name = temps_dict[name]
@@ -137,4 +159,10 @@ if __name__ == "__main__":
 
     except Exception as e:
         print(f"Something went wrong. Please refer to exception log:\n{e}")
+        print("Exiting in 10 seconds (You can close this window)")
+        sleep(10)
         exit(4)
+
+    print("Finished! Exiting in 5 seconds...")
+    sleep(5)
+    
