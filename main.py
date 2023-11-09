2dac270bef045d5ab3eaa24d10e55959263e8ffe
Thierry Laforge <34071949+pakwan8@users.noreply.github.com>
2023-11-09 15:51:12 -0700

    Massive speed improvements

--- main.py
+++ main.py
@@ -1,191 +1,36 @@
-import cv2
-import json
-import gspread
-import subprocess as sp
-from sys import exit
-from time import sleep
-from base64 import b64decode, b64encode
+from tools import *
+from threading import Timer
 from datetime import datetime
-from tools.common import obtain_auth, read_file, get_statuses
-
-
-VERBOSE = False
-SLEEP_TIME = 1
-
-
-def run_update() -> sp.Popen | None:
-    print("Starting update procedure...")
-    print(f"Running with {len(batch)} entries.")
-    if len(batch) == 0:
-        print("No entries found in batch")
-
-        if update_mode == 'q':
-            exit("No errors occurred on main script.")
-
-        elif update_mode == 'u':
-            return None
-
-    packaged = b64encode(" -- ".join([" | ".join(i) for i in batch]).encode()).decode()
-    final_statuses = b64encode(json.dumps(statuses).encode()).decode()
-    try:
-        task = sp.Popen(
-            f"./venv/bin/python ./tools/sheet_updater.py {packaged} {final_statuses}".split()
-        )
-
-    except FileNotFoundError:
-        task = sp.Popen(
-            f"./venv/Scripts/python.exe ./tools/sheet_updater.py {packaged} {final_statuses}".split()
-        )
-
-    return task
-
-
-def read_code(message: str) -> str:
-    cam = cv2.VideoCapture(0)
-    while True:
-        try:
-            _, frame = cam.read()
-            data, _, _ = reader.detectAndDecode(frame)
-
-        except cv2.error:
-            continue
-
-        if len(data) > 0:
-            cam.release()
-            return data
-
-        frame = cv2.flip(frame, 1)
-        cv2.putText(
-            frame,
-            message,
-            [10, 30],
-            cv2.FONT_HERSHEY_DUPLEX,
-            0.75,
-            [0, 0, 0],
-            1,
-            cv2.LINE_AA,
-        )
-        cv2.putText(
-            frame,
-            "Press 'q' to quit.",
-            [10, 60],
-            cv2.FONT_HERSHEY_DUPLEX,
-            0.75,
-            [0, 0, 0],
-            1,
-            cv2.LINE_AA,
-        )
-        cv2.putText(
-            frame,
-            "Press 'u' to force update.",
-            [10, 90],
-            cv2.FONT_HERSHEY_DUPLEX,
-            0.75,
-            [0, 0, 0],
-            1,
-            cv2.LINE_AA,
-        )
-        cv2.imshow("cbTracker", frame)
-
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            update_mode = 'q'
-            cam.release()
-            cv2.destroyAllWindows()
-            proc = run_update()
-            if proc is not None:
-                while proc.poll() is None:
-                    sleep(0.5)
-
-            exit("No errors occurred on main script.")
-
-        elif key == ord('u'):
-            update_mode = 'u'
-            run_update()
-            batch.clear()
-
 
 if __name__ == "__main__":
-    temps = []
-    room = None
-    update_mode = 'u'
-    reader = cv2.QRCodeDetector()
-    decrypt = {v: k for k, v in read_file("./resources/validations/validation.json").items()}
-
-    cv2.namedWindow("cbTracker", cv2.WINDOW_GUI_NORMAL)
-
-    if VERBOSE is True:
-        print("Reading settings")
-
-    try:
-        for line in read_file("./resources/settings.txt"):
-            if "QR Code" in line:
-                temps.append(line)
-
-            elif "Room number" in line:
-                room = line.split("Room number: ")[1]
-
-            else:
-                raise ValueError
-
-    except ValueError:
-        print("Invalid settings file. Please review and try again.")
-        input("Press ENTER to exit...")
-        exit("Formatting error.")
-
-    client = gspread.service_account_from_dict(
-        json.loads(b64decode(obtain_auth("./resources/gspread_auth.txt")))
-    )
-    sheet = client.open("Chromebook Tracker").worksheet(f"SF{room}")
-    statuses = get_statuses(sheet)
-    status_cells = {f"SF{room}-{i}": f"8{i + 1}" for i in range(1, 7, 1)}
-    status_cells.update({f"SFRED-{i}": f"8{i + 7}" for i in range(1, 33, 1)})
-    temps_dict = {
-        f"student{i + 1}": val.split(":")[1].strip() for i, val in enumerate(temps)
-    }
-    status_rev = {"OUT": "IN", "IN": "OUT"}
-    last_row = len(sheet.col_values(1))
-
-    if VERBOSE is True:
-        print("Opening camera")
-
-    global batch
-    batch = []
-    statuses = get_statuses(sheet)
+    break_flag = 0
+    cam = cv2.VideoCapture(0)
+    decoder = cv2.QRCodeDetector()
+    proc_img = cv2.imread("./resources/img/scan_img.jpg")
+    unhash: dict = read_json("./resources/data/validation.json")
+    settings: dict = read_json("./resources/data/settings.json")
+    chromebooks: dict = read_json("./resources/data/status.json")
+    batch: dict | list = read_json("./resources/data/batch.json", exit_on_err=False)
+    (update_queue := Timer(settings["update"], update_sheet, args=[settings["update"]])).start()
+    print("[INFO] Timer started")
     while True:
-        chromebook = read_code("Please show chromebook")
-        sleep(SLEEP_TIME)
-        encoded = read_code("Please show ID")
-        sleep(SLEEP_TIME)
+        try:
+            chromebook, action = read_code(cam, decoder, "Show Chromebook", unhash, chromebooks, proc_img)
+            student = read_code(cam, decoder, "Show ID", unhash, chromebooks, proc_img)
 
-        if VERBOSE is True:
-            print("Verifying inputs")
+            current_time = datetime.now()
+            batch.append({
+                "action": action,
+                "chromebook": chromebook,
+                "date": f"{current_time.day}/{current_time.month}/{current_time.year}",
+                "student": student,
+                "time": f"{current_time.hour}:{current_time.minute}:{current_time.second}"
+            })
 
-        if encoded not in decrypt:
-            print("QR code not recognized. Please get teacher to look into this.")
-            print(f"Read: {encoded}. Restarting in 3")
-            sleep(3)
-            continue
+            write_json("./resources/data/batch.json", batch)
 
-        if encoded in temps_dict:
-            encoded = temps_dict[encoded]
+        except cv2.error as err:
+            pass
 
-        date = datetime.today()
-        statuses[chromebook] = status_rev[statuses[chromebook]]
-        last_row += 1
-        batch.append(
-            [
-                room,
-                chromebook,
-                statuses[chromebook],
-                decrypt[encoded],
-                f"{f'{date.month:02d}'}/{f'{date.day:02d}'}/{f'{date.year:02d}'}",
-                f"{f'{date.hour:02d}'}:{f'{date.minute:02d}'}:{f'{date.second:02d}'}",
-                str(last_row),
-                status_cells[chromebook],
-            ]
-        )
-
-        if len(batch) >= 3:
-            run_update()
-            batch.clear()
+        except Exception as err:
+            print(f"[ERROR] {err}")
---
+++ tools.py
@@ -0,0 +1,125 @@
+import json
+import gspread
+import cv2
+import numpy as np
+from threading import Timer
+
+
+def read_json(path: str, exit_on_err=True) -> dict | list:
+    try:
+        with open(path, "r") as f:
+            return json.load(f)
+
+    except FileNotFoundError:
+        print(f"[ERROR] File {path} could not be found.")
+        exit(1)
+
+    except json.JSONDecodeError:
+        print(f"[ERROR] File {path} is not valid or is empty.")
+        if exit_on_err:
+            exit(1)
+        else:
+            return []
+
+
+def write_json(path: str, data: dict) -> None:
+    with open(path, "w") as f:
+        try:
+            json.dump(data, f, indent=4)
+
+        except Exception as err:
+            print(f"[ERROR] Could not write json -> {err}")
+
+
+def update_sheet(update_interval) -> None:
+    print("[INFO] Updating sheet")
+
+    # Read files
+    batch: dict = read_json("./resources/data/batch.json", exit_on_err=False)
+    statuses: dict = read_json("./resources/data/status.json")
+
+    # Spreadsheet api setup
+    client = gspread.service_account_from_dict(read_json("./resources/data/api_key.json"))
+    sheet = client.open("Chromebook Tracker").worksheet(f"testing")
+
+    # Get all cells to be modified
+    last_row = len(sheet.col_values(1)) + 1
+    status_cells = sheet.range(f"H2:{len(sheet.col_values(8))}")
+    info_grid = [sheet.range(f"A{last_row + i}:E{last_row + i}") for i in range(len(batch))]
+
+    # Set the batch cells
+    for i, row in enumerate(info_grid):
+        row[0].value = batch[i]["chromebook"]
+        row[1].value = batch[i]["action"]
+        row[2].value = batch[i]["student"]
+        row[3].value = batch[i]["date"]
+        row[4].value = batch[i]["time"]
+
+    # Set the status cells
+    for i, val in enumerate(statuses.values()):
+        status_cells[i].value = val
+
+    # Merge the two together to update all at once
+    for i in info_grid:
+        status_cells.extend(i)
+
+    # Update all modified cells
+    sheet.update_cells(status_cells)
+
+    # Eat batch values
+    with open("./resources/data/batch.json", "w"):
+        pass
+
+    print("[INFO] Finished updating sheet")
+    print("[INFO] Restarting timer")
+    (update_queue := Timer(update_interval, update_sheet, args=[update_interval])).start()
+
+
+def show_proc_img(proc_img: np.array, msg: str) -> None:
+    img = cv2.putText(proc_img.copy(), msg,
+                      [10, 30], cv2.FONT_HERSHEY_DUPLEX, 0.75,
+                      [0, 0, 0], 1, cv2.LINE_AA)
+    cv2.imshow("Scanner", img)
+
+
+def read_code(cam: cv2.VideoCapture, decoder: cv2.QRCodeDetector,
+              msg: str, hash_dict: dict, status_dict: dict,
+              proc_img: np.array) -> str | tuple[str, str]:
+    while True:
+        _, raw_frame = cam.read()
+        raw_result, _, _ = decoder.detectAndDecode(raw_frame)
+        status_flip = {"IN": "OUT", "OUT": "IN"}
+        if raw_frame is None:
+            print("[ERROR] Could not read camera")
+            cv2.destroyAllWindows()
+            cam.release()
+            exit(1)
+
+        else:
+            if raw_result == "":
+                frame = cv2.flip(raw_frame, 1)
+                cv2.putText(frame, msg,
+                            [10, 30], cv2.FONT_HERSHEY_DUPLEX, 0.75,
+                            [0, 0, 0], 1, cv2.LINE_AA)
+                cv2.imshow("Scanner", frame)
+
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord('q'):
+                    cv2.destroyAllWindows()
+                    cam.release()
+                    exit(0)
+
+            else:
+                if raw_result in hash_dict:
+                    show_proc_img(proc_img.copy(), f"Obtained: {(unhashed := hash_dict[raw_result])}")
+                    if cv2.waitKey(1000) & 0xFF == 27:
+                        pass
+                    return unhashed
+
+                elif raw_result in status_dict:
+                    status_dict[raw_result] = status_flip[status_dict[raw_result]]
+                    write_json("./resources/data/status.json", status_dict)
+                    show_proc_img(proc_img.copy(), f"Obtained: {raw_result}")
+                    if cv2.waitKey(1000) & 0xFF == 27:
+                        pass
+                    return raw_result, status_dict[raw_result]
