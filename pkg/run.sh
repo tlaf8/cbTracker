1cfcc1b3ce03e33b3f7193b3a23bdc79dda0bc1b
Thierry Laforge <21laforgth@gmail.com>
2023-11-20 11:22:41 -0700

    Package for distribution

---
+++ pkg/run.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+cd $HOME/Documents/cbTracker/
+venv/bin/python main.py
+read -n 1 -s -r -p "Press any key to continue"
---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "seeing if $HOME/Documents/cbTracker/ exists"
+if [ ! -d "$HOME/Documents/cbTracker/" ]; then
+  echo "does not exist. Creating"
+  mkdir $HOME/Documents/cbTracker
+fi
+
+echo "copying files over"
+cp -r pkg/* $HOME/Documents/cbTracker
+
+echo "installing virtual environment and necessary libs"
+cd $HOME/Documents/cbTracker
+python -m venv venv
+venv/bin/python -m pip install --upgrade pip
+venv/bin/pip install -r resources/requirements.txt
+
+
+echo "creating desktop shortcut"
+
+touch $HOME/Desktop/cbTracker.desktop
+echo "" > $HOME/Desktop/cbTracker.desktop
+echo "[Desktop Entry]" >> $HOME/Desktop/cbTracker.desktop
+echo "Type=Application" >> $HOME/Desktop/cbTracker.desktop
+echo "Version=1.0" >> $HOME/Desktop/cbTracker.desktop
+echo "Name=cbTracker" >> $HOME/Desktop/cbTracker.desktop
+echo "Comment=" >> $HOME/Desktop/cbTracker.desktop
+echo "Exec=bash $HOME/Documents/cbTracker/run.sh && read -n 1 -s -r -p 'Press any key to continue'" >> $HOME/Desktop/cbTracker.desktop
+echo "Terminal=true" >> $HOME/Desktop/cbTracker.desktop
+echo "Icon=$HOME/Documents/cbTracker/resources/img/scan_img.jpg" >> $HOME/Desktop/cbTracker.desktop
+
+chmod +x $HOME/Desktop/cbTracker.desktop
+
+printf "\ndone\n"
