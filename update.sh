118d1ec0898466d39967664a81812a7239d1dfd5
pakwan8 <21laforgth@gmail.com>
2023-12-14 19:01:59 -0700

    Add echo msgs to update

--- update.sh
+++ update.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 # Move auth and validation
+echo moving validation and auth key
 mv pkg/resources/data/api_key.json pkg/resources/data/validation.json .
 
 # Remove all old files
+echo purging old files
 rm -r pkg/*
 
 # Download new files
+echo cloning new files
 git clone https://github.com/pakwan8/cbTracker /tmp/cbTracker
 
 # Move new files into pkg and delete tmp
+echo moving new files
 mv /tmp/cbTracker/pkg/* pkg
 yes | rm -r /tmp/cbTracker
 
 # Move validation and auth back
+echo moving validation and auth key back
 mv validation.json api_key.json pkg/resources/data
 
 echo finished
